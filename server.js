ac693da5c6e21928c1097f88a286fdda09693069
FoReVeR-GH <dream.high.dev@gmail.com>
2020-08-25 18:17:38 -0700

    updated api endpoints

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// get all bootcamp
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+// get single bootcamp
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
+});
+
+// post
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Created new bootcamp' });
+});
+
+// put
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Updated bootcamp ${req.params.id}` });
+});
+
+// delete
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Deleted bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 7000;
 
 app.listen(
