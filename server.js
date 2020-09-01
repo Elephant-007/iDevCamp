ba82d503860b671a93641f5d1d9e30aced4dcf17
FoReVeR-GH <dream.high.dev@gmail.com>
2020-09-01 02:30:22 -0700

    updated MongoDB connection to the Atlas Cloud

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
-
-// Route Files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to DB
+connectDB();
+
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-app.use(logger);
+// Dev Logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 7000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // close server & exit process
+  server.close(() => process.exit(1));
+});
