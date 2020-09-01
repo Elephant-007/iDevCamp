4bf9a1302adc2af6909b7eead6c9426fb13a75c2
FoReVeR-GH <dream.high.dev@gmail.com>
2020-09-01 02:40:51 -0700

    added console colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const color = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -26,12 +27,14 @@ const PORT = process.env.PORT || 7000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
 
   // close server & exit process
   server.close(() => process.exit(1));
