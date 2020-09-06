399c468c24b3df1b503831e47407a2c4613b94b5
FoReVeR-GH <dream.high.dev@gmail.com>
2020-09-05 22:40:14 -0700

    updated mongoose validation error

--- middleware/error.js
+++ middleware/error.js
@@ -19,6 +19,12 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 400);
   }
 
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error',
