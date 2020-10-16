f69f2ba402eb7b0668c8eafb4eaf866fd12f2591
FoReVeR-GH <dream.high.dev@gmail.com>
2020-10-16 12:10:56 -0700

    updated select query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,16 +8,35 @@ const Bootcamp = require('../models/Bootcamp');
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Filter out fields that match
+  const removeFields = ['select', 'sort'];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, $lt, $lte)
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
-  console.log(queryStr);
-
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Execute query
   const bootcamps = await query;
 
   res.status(200).json({
