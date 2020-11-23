74f210dd0d0aaa97e0de96566d6313742d8224fb
FoReVeR-GH <dream.high.dev@gmail.com>
2020-11-22 22:04:45 -0800

    implemented mongoose cascade delete functionality for all courses related to a bootcamp when it is deleted

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -133,6 +133,13 @@ BootcampSchema.pre('save', async function (next) {
   next();
 });
 
+// Cascade delete ALL coureses related to a specific bootcamp when it is deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
