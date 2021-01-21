97efe9a8f4fd206c9c6eb1ca857bf5f03985bd8d
FoReVeR-GH <dream.high.dev@gmail.com>
2021-01-20 19:17:43 -0800

    updated port from 7000 to 5000

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
