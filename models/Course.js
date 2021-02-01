b87cb6aa237e3c97ee80e109c2f5394448eccbe0
FoReVeR-GH <dream.high.dev@gmail.com>
2021-02-01 13:46:38 -0800

    updated Bootcamp and Courses model

--- models/Course.js
+++ models/Course.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const CourseSchema = new mongoose.Schema({
   title: {
     type: String,
+    unique: true,
     trim: true,
     required: [true, 'Please add a course title'],
   },
