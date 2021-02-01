2f554bbec26bce945767f948e255e268897bfc87
FoReVeR-GH <dream.high.dev@gmail.com>
2021-02-01 11:10:20 -0800

    uncommented courses seeder

--- models/Course.js
+++ models/Course.js
@@ -40,8 +40,7 @@ const CourseSchema = new mongoose.Schema({
 
 // Static method to get average of course tuitions
 CourseSchema.statics.getAverageCost = async function(bootcampId) {
-  console.log('Calculating average cost...'.blue);
-
+  // console.log('Calculating average cost...'.blue);
   const obj = await this.aggregate([
     {
       $match: {bootcamp: bootcampId}
