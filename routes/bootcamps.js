abc3ecae1b2a3b8ddba1f9909c9793b092ac2cb6
FoReVeR-GH <dream.high.dev@gmail.com>
2021-02-01 12:34:51 -0800

    refactored bootcamp controller so advanced filter is generic and could be reused for other models as well

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,10 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -21,7 +25,10 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses') ,getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')
