dd8f8d5b1041d164244bdf7936a7b651dacb9664
FoReVeR-GH <dream.high.dev@gmail.com>
2021-02-02 12:00:57 -0800

    added role authorization functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -33,3 +33,13 @@ exports.protect = asyncHandler(async(req, res, next) => {
        return next(new ErrorResponse('Not authorized to access this route', 401));
    }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorResponse(`User role of ${req.user.role} is not authorized to access this route`, 403));
+        }
+        next();
+    };
+};
