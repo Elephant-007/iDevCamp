b79bc7bc2b214c664dbc3e826677d37a0988b09b
FoReVeR-GH <dream.high.dev@gmail.com>
2021-02-03 13:42:02 -0800

    updated funtionality to create a user

--- controllers/users.js
+++ controllers/users.js
@@ -4,21 +4,32 @@ const User = require('../models/User');
 
 // @desc    Get all users
 // @route   GET /api/v1/auth/users
-// @access  Public/Admin
+// @access  Private/Admin
 exports.getUsers = asyncHandler(async(req, res, next) => {
     res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get single user
 // @route   GET /api/v1/auth/users/:id
-// @access  Public/Admin
+// @access  Private/Admin
 exports.getUser = asyncHandler(async(req, res, next) => {
     // find user
     const user = await User.findById(req.params.id);
-    
-    res
-    .status(200)
-    .json({
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+// @desc    Create users
+// @route   POST /api/v1/auth/users
+// @access  Private/Admin
+exports.createUser = asyncHandler(async(req, res, next) => {
+    // create a user
+    const user = await User.create(req.body);
+
+    res.status(201).json({
         success: true,
         data: user
     });
