431ed5610454d02cf5a502b6dd14ba0b2136de02
FoReVeR-GH <dream.high.dev@gmail.com>
2021-02-05 12:19:43 -0800

    install xss-clean for XSS prevention

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -44,6 +45,9 @@ app.use(mongoSanitize());
 
 // !IMPORTANT --->Set Security Headers<---
 app.use(helmet());
+
+// !IMPORTANT --->Prevent XSS attacks<---
+app.use(xss());
 
 // Set /public/uploads as a static folder
 app.use(express.static(path.join(__dirname, 'public')));
